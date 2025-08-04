ce1377dfbe2974ccaecb6e1673729e3ccb532e5f
Kevin Tobler <internet@kevintobler.ch>
2025-08-04 21:54:50 +0200

    Update download.php

--- download.php
+++ download.php
@@ -35,31 +35,38 @@ if (!empty($info['password'])) {
     }
 
     if (!$pwValid) {
-        $errorMsg = $pwError ? "<span style='color:red; font-weight:bold;'>{$t['wrong_password']}</span><br><br>" : "";
+        $errorMsg = $pwError ? "<span>{$t['wrong_password']}</span><br><br>" : "";
         echo "<!DOCTYPE html><html lang='$lang'><head><meta charset='UTF-8' name='viewport' content='width=device-width, initial-scale=0.6'><title>{$t['title']} - {$t['password_required']}</title>
         <link rel='stylesheet' href='inc/style.css'>
         </head><body>
         <h2>{$t['title']} - {$t['password_required']}</h2>
             <form method='post'>
-            $errorMsg
+			<div id='languageFlags' style='font-size: 2em; cursor: pointer; user-select: none;'>
+				<span id='flag-de' title='German' onclick='changeLang(\"de\")' style='margin-right: 10px; " . ($lang === 'de' ? '' : 'opacity:0.5;') . "'>🇩🇪</span>
+				<span id='flag-en' title='English' onclick='changeLang(\"en\")' style='margin-right: 10px; " . ($lang === 'en' ? '' : 'opacity:0.5;') . "'>🇬🇧</span>
+				<span id='flag-fr' title='Français' onclick='changeLang(\"fr\")' style='margin-right: 10px; " . ($lang === 'fr' ? '' : 'opacity:0.5;') . "'>🇫🇷</span>
+				<span id='flag-it' title='Italiano' onclick='changeLang(\"it\")' style='" . ($lang === 'it' ? '' : 'opacity:0.5;') . "'>🇮🇹</span>
+			</div>
                 <input type='hidden' name='pw_token' value='" . htmlspecialchars($token) . "'>
                 <input type='password' name='pw_input' placeholder='{$t['enter_password']}' required autofocus>
                 <br><br>
                 <button type='submit'>{$t['start_download']}</button>
             </form>
+            <br><br>
+            <p>$errorMsg</p>
             <footer>{$t['title']} {$t['version']} {$t['footer_text']}</footer>
+            <script src='js/main.js'></script>
         </body></html>";
         exit;
     }
 }
 
-// Download markieren
+// mark Download
 if ($info['type'] === 'once') {
     $fileData[$token]['used'] = true;
     file_put_contents($dataFile, json_encode($fileData, JSON_PRETTY_PRINT));
 }
 
-// Keine weitere Ausgabe! Nur Header + Datei
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="' . basename($info['name']) . '"');
