09a52e327acd149f498b5d902d4928fa3f8eea55
Kevin Tobler <internet@kevintobler.ch>
2026-06-09 20:32:03 +0200

    Update to version 3.2

--- save_emailadress.php
+++ save_emailadress.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'inc/config.php';
+require_once 'inc/helpers.php';
 
 header('Content-Type: text/plain; charset=utf-8');
 
@@ -23,27 +24,35 @@ $uploadId      = trim($input['uploadId'] ?? '');
 $uploader      = trim($input['uploader_email'] ?? '');
 $recipientRaw  = trim($input['recipient_email'] ?? '');
 
+if (!preg_match('/^[a-f0-9]{16}$/', $uploadId)) {
+    http_response_code(400);
+    exit('Invalid uploadId');
+}
+
+if ($uploader !== '' && !filter_var($uploader, FILTER_VALIDATE_EMAIL)) {
+    http_response_code(400);
+    exit('Invalid uploader email');
+}
+
 $recipients = preg_split('/[\s,;]+/', $recipientRaw);
 $recipients = array_unique(array_filter(array_map('trim', $recipients), function ($email) {
-	return filter_var($email, FILTER_VALIDATE_EMAIL);
+return filter_var($email, FILTER_VALIDATE_EMAIL);
 }));
 
-$fileData = file_exists($dataFile) ? json_decode(file_get_contents($dataFile), true) : [];
-if (!isset($fileData[$uploadId])) {
-	$fileData[$uploadId] = [
-		'uploader_email'  => $uploader,
-		'recipient_email' => $recipients,
-		'keys'            => []
-	];
-}
+$saved = update_json_file($dataFile, function (array $fileData) use ($uploadId, $uploader, $recipients): array {
+    $fileData[$uploadId] = [
+        'uploader_email'  => $uploader,
+        'recipient_email' => $recipients,
+        'keys'            => isset($fileData[$uploadId]['keys']) && is_array($fileData[$uploadId]['keys'])
+            ? $fileData[$uploadId]['keys']
+            : []
+    ];
+    return $fileData;
+});
 
-if (!isset($fileData[$uploadId]['keys']) || !is_array($fileData[$uploadId]['keys'])) {
-    $fileData[$uploadId]['keys'] = [];
+if (!$saved) {
+    http_response_code(500);
+    exit('Could not save upload metadata');
 }
-
-if (!in_array($key, $fileData[$uploadId]['keys'])) {
-    $fileData[$uploadId]['keys'][] = $key;
-}
-
-file_put_contents($dataFile, json_encode($fileData, JSON_PRETTY_PRINT));
 http_response_code(200);
+echo 'OK';
